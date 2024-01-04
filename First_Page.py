d85e514b121de84ee728e689ea1e25f5978e9fe3
secr <ch.liefrid@gmail.com>
2024-01-04 15:29:27 +0100

    follow POO

--- First_Page.py
+++ First_Page.py
@@ -1,28 +1,36 @@
 import subprocess
-from tkinter import *
-w=Tk()
-w.geometry('300x148')
-def btn(x,y,text,bcolor,fcolor,path):
-    def on_enter(e):
-        mybutton['background']=bcolor
-        mybutton['foreground']=fcolor
-    def on_leave(e):
-        mybutton['background']=fcolor
-        mybutton['foreground']=bcolor
+from tkinter import Tk, Button
+import tkinter as tk
+class First_Page(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root, bg="lightblue")
+        self.root = root
+        self.root.geometry('300x148')
+        self.root.configure(bg='#040c0c')
 
-    def on_click():
-        # Run the external script when the button is clicked
-        subprocess.run(["python", path])
-    mybutton = Button(w, width=43, height=2, text=text,fg=bcolor,bg=fcolor,border=0,activebackground=bcolor,activeforeground=fcolor,command=on_click)
-    mybutton.bind('<Enter>',on_enter)
-    mybutton.bind('<Leave>',on_leave)
-    mybutton.place(x=x, y=y)
+        self.create_button(0, 0, "Créer les données", '#24baa3', '#197069', 'createdata.py')
+        self.create_button(0, 37, "Importer un fichier des données", '#24baa3', '#197069', 'ImporterData.py')
+        # self.create_button(0, 74, "Représnter les données", '#24baa3', '#197069', 'RepData.py')
+        # self.create_button(0, 111, "Gestion des données", '#24baa3', '#197069', 'manageData.py')
+        # self.create_button(0, 148, "Algorithmes de machine learning", '#24baa3', '#197069', 'algo.py')
 
-w.configure(bg='#040c0c')
-btn(0,0,"Créer les données",'#24baa3','#197069','createdata.py')
-btn(0,37,"Importer un fichier des données",'#24baa3','#197069','ImporterData.py')
-btn(0,74,"Représnter les données",'#24baa3','#197069','RepData.py')
-btn(0,111,"Gestion des données",'#24baa3','#197069','manageData.py')
-btn(0,148,"Algorithmes de machine learning",'#24baa3','#197069','algo.py')
+    def create_button(self, x, y, text, bcolor, fcolor, path):
+        def on_enter(e):
+            my_button['background'] = bcolor
+            my_button['foreground'] = fcolor
 
-w.mainloop()
+        def on_leave(e):
+            my_button['background'] = fcolor
+            my_button['foreground'] = bcolor
+
+        def on_click():
+            # Run the external script when the button is clicked
+            subprocess.run(["python", path])
+
+        my_button = Button(self.root, width=43, height=2, text=text, fg=bcolor, bg=fcolor,
+                           border=0, activebackground=bcolor, activeforeground=fcolor, command=on_click)
+        my_button.bind('<Enter>', on_enter)
+        my_button.bind('<Leave>', on_leave)
+        my_button.place(x=x, y=y)
+
+
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import tkinter as tk
 
+from First_Page import First_Page
+
+
+class Main(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Main")
+        self.geometry("400x300")
+        self.firstPage=First_Page(self)
+        self.firstPage.pack(fill=tk.BOTH, expand=True)
+
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.mainloop()
